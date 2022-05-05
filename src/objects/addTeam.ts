3ac42039df2eeb95a146c793fdd5a180b4dc261b
Nina Hahn <hahnap4@gmail.com>
2022-05-04 23:52:49 -0500

    Added objects for /newteam/1

--- src/objects/addTeam.ts
+++ src/objects/addTeam.ts
@@ -3,12 +3,35 @@ import { Locator, Page } from '@playwright/test';
 export class addTeamPage {
   readonly page: Page;
 
-  //
+  // Existing Organization
+  readonly radioBtnForExistingOrg: Locator;
+  readonly orgOption: Locator;
+  readonly myOrgOption: Locator;
+
+  // New Organization
+  readonly radioBtnForNewOrg: Locator;
+  readonly newOrgName: Locator;
+
+  // General
+  readonly teamName: Locator;
+  readonly createTeam: Locator;
   
   constructor(page: Page) {
     this.page = page;
 
-    //
+  // Existing Organization
+    this.radioBtnForExistingOrg = page.locator('.css-ohjobq >> text=an existing organization:');
+    this.orgOption = page.locator('.css-914c56');
+    this.myOrgOption = page.locator('role=menuitem >> .css-5w21bi');
+
+  // New Organization
+    this.radioBtnForNewOrg = page.locator('.css-ohjobq >> text=a new organization:');
+    this.newOrgName = page.locator('.css-kxtm4u >> name=orgName');
+
+  // General
+    this.teamName = page.locator('.css-kxtm4u >> name=teamName');
+    this.createTeam = page.locator('button:has-text("Create Team & Org")');
+
   }
 
 }
---
+++ src/objects/createMeeting.ts
@@ -0,0 +1,16 @@
+import { Locator, Page } from '@playwright/test';
+
+export class createMeetingPage {
+  readonly page: Page;
+
+  // 
+  
+  constructor(page: Page) {
+    this.page = page;
+
+  // 
+
+  }
+
+}
+
