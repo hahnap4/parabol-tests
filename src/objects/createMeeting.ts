7952ba00240d1de26c38e9ac103c323ee8f521f3
Nina Hahn <hahnap4@gmail.com>
2022-05-05 18:52:26 -0500

    Added readonly locator declarations to objects in creatMeeting

--- src/objects/createMeeting.ts
+++ src/objects/createMeeting.ts
@@ -3,12 +3,27 @@ import { Locator, Page } from '@playwright/test';
 export class createMeetingPage {
   readonly page: Page;
 
-  // 
+  // General
+  readonly learnMore: Locator;
+  readonly startMeeting: Locator;
+  readonly rightButton: Locator;
+  readonly leftButton: Locator;
+  readonly backPageButton: Locator;
+
+  // Settings for Meeting Creation
+  readonly teamOptions: Locator;
+  readonly myTeam: Locator;
+  readonly templateEdit: Locator;
+  readonly includeIcebreaker: Locator;
+
+  // Templates
   
   constructor(page: Page) {
     this.page = page;
 
-  // 
+  // Settings for Meeting Creation
+
+  // Templates
 
   }
 
