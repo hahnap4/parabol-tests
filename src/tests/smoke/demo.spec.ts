cf96fd374b1c236411e16b7f67171c32bd79c4f5
Nina Hahn <hahnap4@gmail.com>
2022-05-11 20:15:52 -0500

    Started the demo tests

--- src/tests/smoke/demo.spec.ts
+++ src/tests/smoke/demo.spec.ts
@@ -1,6 +1,18 @@
+import { test, expect } from '@playwright/test';
+import { inMeetingPage } from '../../objects/inMeeting';
+
+test.describe('Unauthenticated Demo', () => {
+
+    // 
+    test(' ', async({ page }) => {
+        const demopage = new inMeetingPage(page);
+        await page.goto('https://action-staging.parabol.co/retrospective-demo/reflect');
+    });
+    
+});
+
 /* 
 TODO: Test the Demo, unauthenticated
-https://action-staging.parabol.co/retrospective-demo/reflect
 
 Requires: 
  - inMeeting objects
