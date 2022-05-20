9e412bcc9c7ab0105b0cd8036eff07475956e051
Nina Hahn <hahnap4@gmail.com>
2022-05-20 16:50:18 -0500

    Added UI tests for timeline and upgrade to pro pages with masking

--- src/fixtures/testSetup.ts
+++ src/fixtures/testSetup.ts
@@ -1,5 +1,5 @@
 import { test as base } from '@playwright/test';
-import { inMeetingPage } from '@index';
+import { inMeetingPage, orgListPage, meetingHomePage } from '@index';
 
 type MyFixtures = {
   demoReflectPage: inMeetingPage;
@@ -7,6 +7,9 @@ type MyFixtures = {
   demoVotePage: inMeetingPage;
   demoDiscussPage: inMeetingPage;
   endDemoPage: inMeetingPage;
+
+  upgradePlanPage: orgListPage;
+  mobileUpgradePage: orgListPage;
 };
 
 export const test = base.extend<MyFixtures>({
@@ -62,6 +65,27 @@ export const test = base.extend<MyFixtures>({
     await demopage.nextBtn.dblclick();
 
     await use(demopage);
+  },
+
+  // For Desktop
+  upgradePlanPage: async ({ page }, use) => {
+    const orglistpage = new orgListPage(page);
+    await page.goto('/me/organizations');
+    await orglistpage.upgradeToPro.click();
+
+    await use(orglistpage);
+  },
+
+  mobileUpgradePage: async ({ page }, use) => {
+    const orglistpage = new orgListPage(page);
+    const homePage = new meetingHomePage(page);
+    await page.goto('/me/organizations');
+    await homePage.mobileUpgrade.click();
+    await orglistpage.upgradeToPro.isVisible();
+    await orglistpage.upgradeToPro.click();
+    await page.waitForLoadState('networkidle');
+
+    await use(orglistpage);
   },
 });
 
