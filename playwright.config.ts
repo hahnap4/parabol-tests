f58ac70862e81eebaa1b886dcb413e2d7412313a
Nina Hahn <hahnap4@gmail.com>
2022-07-22 14:35:59 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ const config: PlaywrightTestConfig = {
   retries: process.env.CI ? 2 : 0, 
   globalTeardown: './globalTeardown.ts',
   fullyParallel: true,
-  workers: 3,
+  workers: 2,
   timeout: 60000, 
   expect: {
     timeout: 15000,
